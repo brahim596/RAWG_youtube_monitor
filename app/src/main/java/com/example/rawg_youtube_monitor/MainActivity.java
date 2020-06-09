02dba5fb4ff3d07682ee8aca614560b9cd1f4e72
Brahim Djellouli <brahim.dje.59@gmail.com>
2020-06-09 20:26:36 +0200

    set up tablayout navigation and colors application

--- app/src/main/java/com/example/rawg_youtube_monitor/MainActivity.java
+++ app/src/main/java/com/example/rawg_youtube_monitor/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.rawg_youtube_monitor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPage);
     }
 }
