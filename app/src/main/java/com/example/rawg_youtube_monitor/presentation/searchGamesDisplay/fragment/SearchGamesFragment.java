4f214981fa487c7b11569c11ba8e2aee210a6410
brahim596 <brahim.dje.59@gmail.com>
2020-06-12 17:31:40 +0200

    link search fragment to his view elements

--- app/src/main/java/com/example/rawg_youtube_monitor/presentation/searchGamesDisplay/fragment/SearchGamesFragment.java
+++ app/src/main/java/com/example/rawg_youtube_monitor/presentation/searchGamesDisplay/fragment/SearchGamesFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.rawg_youtube_monitor.R;
 import com.example.rawg_youtube_monitor.presentation.favGamesVideoDisplay.fragment.FavGamesVideoFragment;
@@ -15,6 +17,8 @@ import com.example.rawg_youtube_monitor.presentation.favGamesVideoDisplay.fragme
 public class SearchGamesFragment extends Fragment {
 
     private View view;
+    private RecyclerView searchGamesResultRecyclerView;
+    private EditText searchGamesEditText;
 
     public SearchGamesFragment() {}
 
@@ -27,6 +31,8 @@ public class SearchGamesFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         view =inflater.inflate(R.layout.fragment_search_games,container,false);
+        this.searchGamesResultRecyclerView = view.findViewById(R.id.searchGamesResultRecyclerView);
+        this.searchGamesEditText = view.findViewById(R.id.searchGamesEditText);
         return view;
     }
 
