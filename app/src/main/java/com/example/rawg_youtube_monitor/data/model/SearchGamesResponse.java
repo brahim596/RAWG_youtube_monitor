abd50d92bbeba030353711b4932c9c8785410b24
brahim596 <brahim.dje.59@gmail.com>
2020-06-12 14:26:09 +0200

    add model objects response api

---
+++ app/src/main/java/com/example/rawg_youtube_monitor/data/model/SearchGamesResponse.java
@@ -0,0 +1,10 @@
+package com.example.rawg_youtube_monitor.data.model;
+
+import java.util.List;
+
+public class SearchGamesResponse {
+    int count;
+    String next;
+    String previous;
+    List<Game> results;
+}
---
+++ app/src/main/java/com/example/rawg_youtube_monitor/data/service/SearchGamesDisplayService.java
@@ -0,0 +1,5 @@
+package com.example.rawg_youtube_monitor.data.service;
+
+public interface SearchGamesDisplayService {
+
+}
