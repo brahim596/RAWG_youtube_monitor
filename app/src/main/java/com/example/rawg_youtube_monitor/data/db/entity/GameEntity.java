3f603b7451ab0404a65c203bdc515ac8cd13ef4e
brahim596 <brahim.dje.59@gmail.com>
2020-06-12 16:02:54 +0200

    fix game entity

--- app/src/main/java/com/example/rawg_youtube_monitor/data/db/entity/GameEntity.java
+++ app/src/main/java/com/example/rawg_youtube_monitor/data/db/entity/GameEntity.java
@@ -15,4 +15,36 @@ public class GameEntity {
     String name;
     int rating;
 
+    @NonNull
+    public String getId() {
+        return id;
+    }
+
+    public void setId(@NonNull String id) {
+        this.id = id;
+    }
+
+    public String getBackground_image() {
+        return background_image;
+    }
+
+    public void setBackground_image(String background_image) {
+        this.background_image = background_image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 }
