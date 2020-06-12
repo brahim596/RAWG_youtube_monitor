c72f04f110533ab1bd257a3d6223f000b45c284d
brahim596 <brahim.dje.59@gmail.com>
2020-06-12 16:17:55 +0200

    add mapping methods for game entity

--- app/src/main/java/com/example/rawg_youtube_monitor/data/model/Game.java
+++ app/src/main/java/com/example/rawg_youtube_monitor/data/model/Game.java
@@ -10,4 +10,76 @@ public class Game {
     int rating;
     int rating_top;
     int ratings_count;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getReleased() {
+        return released;
+    }
+
+    public void setReleased(String released) {
+        this.released = released;
+    }
+
+    public String getBackground_image() {
+        return background_image;
+    }
+
+    public void setBackground_image(String background_image) {
+        this.background_image = background_image;
+    }
+
+    public boolean isTba() {
+        return tba;
+    }
+
+    public void setTba(boolean tba) {
+        this.tba = tba;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getRating_top() {
+        return rating_top;
+    }
+
+    public void setRating_top(int rating_top) {
+        this.rating_top = rating_top;
+    }
+
+    public int getRatings_count() {
+        return ratings_count;
+    }
+
+    public void setRatings_count(int ratings_count) {
+        this.ratings_count = ratings_count;
+    }
 }
--- app/src/main/java/com/example/rawg_youtube_monitor/data/repository/games/GamesDataRepository.java
+++ app/src/main/java/com/example/rawg_youtube_monitor/data/repository/games/GamesDataRepository.java
@@ -6,6 +6,9 @@ import com.example.rawg_youtube_monitor.data.model.SearchGamesResponse;
 import com.example.rawg_youtube_monitor.data.repository.games.local.GamesLocalDataSource;
 import com.example.rawg_youtube_monitor.data.repository.games.remote.GamesRemoteDataSource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.reactivex.Single;
 
 public class GamesDataRepository implements GamesRepository {
@@ -21,4 +24,44 @@ public class GamesDataRepository implements GamesRepository {
     public Single<SearchGamesResponse> searchGamesByName(String name, int pageSize, int page) {
         return this.gamesRemoteDataSource.searchGameByName(name, pageSize, page);
     }
+
+
+
+    /************* Methods for mapping  (game entity) <---> (game) **************/
+
+
+    private List<GameEntity> mapListGameToListGameEntity(List<Game> games){
+        List<GameEntity> gameEntities = new ArrayList<>();
+        for(Game game:games)
+            gameEntities.add(mapGameToGameEntity(game));
+
+        return gameEntities;
+    }
+
+    private List<Game> mapListGameEntityToListGame(List<GameEntity> gameEntities){
+        List<Game> games = new ArrayList<>();
+        for(GameEntity gameEntity:gameEntities)
+            games.add(mapGameEntityToGame(gameEntity));
+
+        return games;
+    }
+
+    private GameEntity mapGameToGameEntity(Game game){
+        GameEntity gameEntity = new GameEntity();
+        gameEntity.setId(game.getId());
+        gameEntity.setBackground_image(game.getBackground_image());
+        gameEntity.setName(game.getName());
+        gameEntity.setRating(game.getRating());
+        return gameEntity;
+    }
+
+    private Game mapGameEntityToGame(GameEntity gameEntity){
+        Game game = new Game();
+        game.setId(gameEntity.getId());
+        game.setBackground_image(gameEntity.getBackground_image());
+        game.setName(gameEntity.getName());
+        game.setRating(gameEntity.getRating());
+        return game;
+    }
+
 }
